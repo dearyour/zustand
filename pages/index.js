4c34d9772f4878bedce1d459370b177b7dc91bb3
김용희 <52556956+dearyour@users.noreply.github.com>
2022-07-04 02:59:57 +0900

    mocks Data

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import ZustandPage from "./zustandPage";
 
+if (process.env.NODE_ENV === "development") {
+  const { worker } = require("../mocks/browser");
+  worker.start();
+}
 export default function Home() {
   return (
     <div className={styles.container}>
